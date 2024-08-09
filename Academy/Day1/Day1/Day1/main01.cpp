49fafde8224b0b9f5b0224195413e0134675b03b
sbscerti <junhobag007@gmail.com>
2024-08-09 21:13:02 +0900

    정리

---
+++ Academy/Day1/Day1/Day1/main01.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+void main()
+{
+	printf("동해물과 백두산이 마르고 닮도록\n대한사람 대한으로 길이 보전하세");
+	printf("하느님이 보우하사 우리나라 만세\n"); 
+	printf("무궁화 삼천리 화려강산\n");
+	printf("대한사람 대한으로 길이 보전하세");
+}
