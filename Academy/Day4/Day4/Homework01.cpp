c683c746db3b814355e24b24a6e57d0f71ed26c7
sbscerti <160083857+sbscerti@users.noreply.github.com>
2024-08-19 21:42:42 +0900

    Day04 진도 & 숙제

---
+++ Academy/Day4/Day4/Homework01.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+using namespace std;
+
+void main()
+{
+	srand(time(NULL));
+
+	int randomNum[1] = rand() % 45;
+	int rotteryNum[45] = {};
+	//rotteryNum[0] ~ rotteryNum[5]
+	//rotteryNum[6]
+	printf("당신의 로또번호는");
+
+	for (int i = 0; i < 10000; i++)
+	{
+		int temp = (int)rotteryNum[randomNum[0]];
+		rotteryNum[randomNum[0]] = rotteryNum[randomNum[1]];
+	}
+
+	for (int j = 0; j < 7; j++)
+	{
+		printf(",%d", rotteryNum[j]);
+	}
+	printf("입니다");
+		
+
+}
---
+++ Academy/Day4/Day4/main02.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+using namespace std;
+
+
+void main()
+{
+
+}
