bf784cdbe5e817f7877c2974ff26553778943cc5
junos205 <junhobag007@gmail.com>
2024-08-21 16:45:03 +0900

    Day04 숙제

--- Academy/Day4/Day4/Homework01.cpp
+++ Academy/Day4/Day4/Homework01.cpp
@@ -9,22 +9,39 @@ void main()
 {
 	srand(time(NULL));
 
-	int randomNum[1] = {};
-	randomNum[0] = rand() % 45;
-	int rotteryNum[45] = {};
+	int randomNum[2] = {};
+	
+	int rotteryNum[46] = {};
 	//rotteryNum[0] ~ rotteryNum[5]
 	//rotteryNum[6]
 	printf("당신의 로또번호는");
+	
+
+
+	for (int k = 0; k < 45; k++)
+	{
+		rotteryNum[k+1] = (int)rotteryNum[k] + 1;
+		rotteryNum[k] = (int)rotteryNum[k] + 1;
+	}
 
 	for (int i = 0; i < 10000; i++)
 	{
-		int temp = (int)rotteryNum[randomNum[0]];
+		randomNum[0] = rand() % 45 + 1;
+
+		randomNum[1] = rand() % 45 + 1;
+		int temp = rotteryNum[randomNum[0]];
 		rotteryNum[randomNum[0]] = rotteryNum[randomNum[1]];
+		rotteryNum[randomNum[1]] = temp;
 	}
 
 	for (int j = 0; j < 7; j++)
 	{
-		printf(",%d", rotteryNum[j]);
+
+		printf(" %d", rotteryNum[j]);
+		if (j <= 5)
+		{
+			printf(",");
+		}
 	}
 	printf("입니다");
 		
